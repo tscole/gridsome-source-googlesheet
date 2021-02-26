0928af2e3ff773cbc284ff431b3f0ab277e7c113
Tim Cole <tscole@inspiredwd.com>
2021-02-26 10:08:46 +0000

    Initial commit

---
+++ file.js
@@ -0,0 +1,113 @@
+/* Usage */
+/*
+const file = require('./file.js')
+
+const imageDirectory = 'tmp_images'
+const images = {}
+let loadImages = false
+
+// Somewhere on each possible image url
+if (url && String(url).match(/^https:\/\/.*\/.*\.(jpg|png|svg|jpeg)($|\?)/i)) {
+  const filename = file.getFilename(url)
+  const id = makeUid(url)
+  const filepath = file.getFullPath(imageDirectory, filename)
+  if (!images[id]) images[id] = {
+    filename,
+    url,
+    filepath
+  }
+
+  loadImages = true
+
+  // Assign filepath as the new value for the image
+  obj[imgField] = filepath
+}
+
+// After processing all fields
+if (loadImages) await downloadImages(images)
+
+async function downloadImages(images) {
+  file.createDirectory(imageDirectory)
+
+  let exists = 0
+  const download = []
+  Object.keys(images).forEach(async (id) => {
+    const { filename, filepath } = images[id]
+
+    if (!file.exists(filepath)) {
+      download.push(images[id])
+    } else exists++
+  })
+
+  ISDEV && console.log(`${exists} images already exists and ${download.length} to download`)
+
+  if (download.length) {
+    await pMap(download, async ({ filename, url, filepath }) => {
+      await file.download(url, filepath)
+      ISDEV && console.log(`Downloaded ${filename}`)
+    }, {
+      concurrency: os.cpus * 2
+    })
+  }
+}
+*/
+
+const https = require('https')
+const fs = require('fs-extra')
+const path = require('path')
+
+const TMPDIR = '.temp/downloads'
+let tmpCount = 0
+
+function createDirectory(dir) {
+  const pwd = path.resolve(dir)
+  if (!fs.existsSync(pwd)) fs.mkdirs(pwd)
+
+  return pwd
+}
+
+createDirectory(TMPDIR)
+
+function getFullPath(dir, filename) {
+  return path.resolve(dir, filename)
+}
+
+function getFilename(url, regex) {
+  let name = url.replace(/%2F/g, '/').split('/').pop().replace(/\#(.*?)$/, '').replace(/\?(.*?)$/, '')
+  return regex ? name.replace(regex, '$1$2') : name
+}
+
+function exists(filepath) {
+  return fs.existsSync(filepath)
+}
+
+function downloadIfNotExists(url, absPath) {
+  if (!url ||  !path || exists(absPath)) return
+
+  return download(url, absPath)
+}
+
+function download(url, absPath) {
+  return new Promise(function(resolve) {
+    const tmpPath = getFullPath(TMPDIR, `${++tmpCount}.tmp`)
+    const file = fs.createWriteStream(tmpPath)
+    const request = https.get(url, (response) => {
+      response.pipe(file)
+      file.on('finish', () => {
+        file.close()
+        fs.rename(tmpPath, absPath, resolve)
+      })
+    }).on('error', (err) => {
+      console.error(err.message)
+      fs.unlink(String(tmpPath), resolve)
+    })
+  })
+}
+
+module.exports = {
+  createDirectory,
+  getFullPath,
+  getFilename,
+  exists,
+  download
+}
---
+++ index.js
@@ -0,0 +1,90 @@
+const { google } = require('googleapis')
+const file = require('./file.js')
+const get = require("async-get-file");
+
+class GoogleSheetSource {
+  static defaultOptions() {
+    return {
+      sheetId: '',
+      apiKey: '',
+      type: 'googleSheet',
+      imageFields: [],
+      imageDirectory: 'googlesheet-images'
+    }
+  }
+
+ constructor(api, options = GoogleSheetSource.defaultOptions()) {
+    this.options = options
+    let range = 'A1:ZZ10000'
+
+    if (this.options.imageFields.length > 0) {
+      file.createDirectory(this.options.imageDirectory)
+    }
+
+    if (this.options.tab) {
+      range = this.options.tab + '!A1:ZZ10000'
+    }
+
+    api.loadSource(async store => {
+      const contentType = store.addCollection({
+        typeName: this.options.type
+      })
+
+      const sheets = google.sheets({
+        version: 'v4',
+        auth: this.options.apiKey,
+      })
+
+      let response = await sheets.spreadsheets.values
+        .get({
+          spreadsheetId: this.options.sheetId,
+          range: range,
+        })
+
+
+          const data = response.data.values
+          const titles = data.shift()
+          const nodes = await data.map(value => {
+            return titles.reduce(
+              (title, key, index) => ({ ...title, [key]: value[index] }),
+              {}
+            )
+          })
+
+          if (this.options.imageFields) {
+            for (let i = 0; i < this.options.imageFields.length; i++) {
+            let field = this.options.imageFields[i]
+              for (let index = 0; index < nodes.length; index++) {
+               let node = nodes[index]
+                if (node[field]) {
+                  let url = node[field]
+                  let filename = file.getFilename(node[field])
+                  let filepath = this.options.imageDirectory + '/' + filename
+                  if (!file.exists(filepath)) {
+                    console.log("Downloading " + node[field])
+                    var options = {
+                        directory: this.options.imageDirectory,
+                        filename: filename
+                      }
+                    await get(node[field],options)
+                  } else {
+                    console.log("Skipping " + node[field] + " file already exists")
+                  }
+                  const id = store.makeUid(filename)
+                  const finalpath = file.getFullPath(this.options.imageDirectory, filename)
+                  node[field] = finalpath
+                }
+              }
+            };
+          }
+
+          nodes.map((value, key, title) => {
+            contentType.addNode(value)
+          })
+
+  })
+  }
+}
+
+
+module.exports = GoogleSheetSource
